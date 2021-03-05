d937b3f0d5fe9d35c0dd565782f3cda3d150a4ce
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2021-03-04 20:31:03 -0800

    added initial code and installed dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Sign from './components/Sign'
+import { Container } from './components/Grid'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}
+    >
+    <Container fluid size="sm-12 md-12">
+      <Sign />
+    </Container>
     </div>
   );
 }
---
+++ src/components/Sign/index.js
@@ -0,0 +1,11 @@
+import comingSoon from '../../assets/comingsoon.jpg'
+
+function Sign() {
+    return (
+      <div className="coming-soon">
+        <img src={ comingSoon } alt="coming-soon" />
+        </div>
+    );
+}
+
+export default Sign;
